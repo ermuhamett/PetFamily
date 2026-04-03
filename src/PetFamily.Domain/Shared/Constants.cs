7d8a4ba4ea1fafc22e9652d71f5fbade2666027d
IT_Бекбосынов Ермухамет Аскарулы <IT_BekbossynovYeA@solidcore-resources.com>
2026-04-03 17:22:33 +0500

    Adding constants for property length

---
+++ src/PetFamily.Domain/Shared/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace PetFamily.Domain.Shared
+{
+    public static class Constants
+    {
+        public const int MAX_LOW_TEXT_LENGTH = 100;
+        public const int MAX_MEDIUM_TEXT_LENGTH = 1000;
+    }
+}
