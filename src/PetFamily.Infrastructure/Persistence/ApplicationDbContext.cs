16492a2eb82d651c2a7b6241801184d2c6f28c43
IT_Бекбосынов Ермухамет Аскарулы <IT_BekbossynovYeA@solidcore-resources.com>
2026-04-03 17:15:31 +0500

    Added DbContext to work database

---
+++ src/PetFamily.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PetFamily.Domain.Pets;
+using PetFamily.Domain.Volunteers;
+
+namespace PetFamily.Infrastructure.Persistence;
+
+public sealed class ApplicationDbContext(IConfiguration configuration) : DbContext
+{
+    public DbSet<Volunteer> Volunteers => Set<Volunteer>();
+    public DbSet<Pet> Pets => Set<Pet>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString("Database"));
+        optionsBuilder.UseSnakeCaseNamingConvention();
+        optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    private ILoggerFactory CreateLoggerFactory() =>
+        LoggerFactory.Create(builder => { builder.AddConsole(); });
+}
